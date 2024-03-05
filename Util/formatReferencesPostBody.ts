0b13d0d3b1f7eb328f74e107ca11b72650963784
david-lorenzo-vargas <dlorenzo.05@gmail.com>
2024-03-05 22:49:58 +0000

    utility created for formating post api call body

---
+++ Util/formatReferencesPostBody.ts
@@ -0,0 +1,23 @@
+import { FormFields } from "../Types/types";
+
+export const formatReferencesPostBody = (obj: FormFields, currentlyWorking: boolean) => {
+  return {
+    personal: {
+      "first_name": obj.firstName,
+      "last_name": obj.lastName,
+      "current_address": obj.personalAddress
+    },
+    employer: [
+      {
+        name: obj.employerName,
+        "start_date": new Date(obj.startDate).getTime(),
+        "end_date": currentlyWorking ? undefined : new Date(obj.endDate).getTime(),
+      }
+    ],
+    guarantor: {
+      name: obj.guarantorName,
+      address: obj.guarantorAddress,
+      relation: obj.relationshipToGuarantor
+    }
+  };
+};
