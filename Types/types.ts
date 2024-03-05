a3813a9e2c9391a9a77ab094cfb09d8b0ded0e4d
david-lorenzo-vargas <dlorenzo.05@gmail.com>
2024-03-05 22:46:51 +0000

    Types file with all types and enums

---
+++ Types/types.ts
@@ -0,0 +1,51 @@
+export type DropdownOption = {
+  key: string;
+  label: string;
+}
+
+type Personal = {
+  "first_name": string,
+  "last_name": string,
+  "current_address": string
+}
+
+type Guarantor = {
+  name: string,
+  address: string,
+  relation?: RelationShipEnum
+}
+
+type Employer = {
+  name: string,
+  "start_date": number,
+  "end_date"?: number,
+}
+
+export type FormFields = {
+  firstName: string,
+  lastName: string,
+  personalAddress: string,
+  employerName: string,
+  startDate: Date,
+  endDate: Date,
+  guarantorName: string,
+  guarantorAddress: string,
+  relationshipToGuarantor: RelationShipEnum | undefined,
+}
+
+export type ReferencesDataObject = {
+  personal: Personal,
+  employer: Employer[],
+  guarantor: Guarantor,
+}
+
+export enum RelationShipEnum {
+  sibling = 'sibling',
+  employer = 'employer',
+  other = 'other'
+}
+
+export enum DateModal {
+  from = 'from',
+  to = 'to'
+}
