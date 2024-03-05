2a90291e72aff41736f91d2e7edda666ddeb5154
david-lorenzo-vargas <dlorenzo.05@gmail.com>
2024-03-05 22:49:08 +0000

    testing for post data form utility function mocking a post api call

---
+++ Util/API/postReferences.test.ts
@@ -0,0 +1,53 @@
+import { ReferencesDataObject, RelationShipEnum } from "../../Types/types";
+import { postData } from "./postReferencesForm";
+
+const mockFetch = jest.fn();
+global.fetch = mockFetch;
+
+const mockData: ReferencesDataObject = {
+  personal: {
+    "first_name": 'first name',
+    "last_name": 'last name',
+    "current_address": 'current address'
+  },
+  employer: [
+    {
+      name: 'employer name',
+      "start_date": new Date().getTime(),
+      "end_date": new Date().getTime(),
+    }
+  ],
+  guarantor: {
+    name: 'guarantor name',
+    address: 'guarantor address',
+    relation: RelationShipEnum.employer,
+  }
+};
+
+describe('postData', () => {
+  it('should post data', async () => {
+    mockFetch.mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        status: 200,
+        json: () => Promise.resolve({ data: [] }),
+      }),
+    );
+    await postData(mockData);
+    expect(mockFetch).toHaveBeenCalledTimes(1);
+    expect(mockFetch).toHaveBeenCalledWith(process.env.POST_URL, {
+      body: JSON.stringify(mockData),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+    });
+  });
+
+  it('should throw an error if promise is rejected', async () => {
+    mockFetch.mockImplementationOnce(() =>
+      Promise.reject());
+
+    await expect(postData(mockData)).rejects.toThrow('Something went wrong');
+  });
+});
