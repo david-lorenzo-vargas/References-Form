1537ae2814197329e1c1786e2f829c81847635ba
david-lorenzo-vargas <dlorenzo.05@gmail.com>
2024-03-06 10:40:33 +0000

    functional component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 import ReferencesForm from "./Components/Molecules/ReferencesForm";
 import GoodLordLogo from "./Components/icons/GoodLordLogo";
 
-
-export default function Home() {
+const Home = () => {
   return (
     <main className="flex h-screen flex-col items-center justify-center bg-veryDarkBlue pb-10">
       <div className="w-full pl-10">
         <GoodLordLogo size="150"/>
       </div>
-      <div className="md:w-1/2 w-85P h-85P overflow-y-scroll flex flex-col items-center bg-white rounded-lg py-10">
+      <div className="md:w-1/2 w-85P h-85P overflow-y-auto flex flex-col items-center bg-white rounded-lg py-10">
         <ReferencesForm />
       </div>
     </main>
   );
 };
+
+export default Home;
