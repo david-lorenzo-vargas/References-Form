ca7501a8455d57ee8ae444a7ef9baaefdf5b51b3
david-lorenzo-vargas <dlorenzo.05@gmail.com>
2024-03-06 10:42:06 +0000

    cleaning up

--- Util/API/postReferencesForm.ts
+++ Util/API/postReferencesForm.ts
@@ -1,21 +1,21 @@
 import { ReferencesDataObject } from "../../Types/types";
 
-export const postData = async(obj: ReferencesDataObject) => {
+export const postData = async (obj: ReferencesDataObject) => {
   if (!process.env.POST_URL) {
-    throw new Error('POST_URL is not defined in environment variables.');
+    throw new Error("POST_URL is not defined in environment variables.");
   }
 
   try {
-    const res = await fetch(process.env.POST_URL , {
+    const res = await fetch(process.env.POST_URL, {
       method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(obj),
     });
-  
+
     return res;
   } catch (error: unknown) {
-    throw new Error('Something went wrong');
+    throw new Error("Something went wrong");
   }
 };
