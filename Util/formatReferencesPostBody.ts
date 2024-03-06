ba5d857da7b882e606d82b07cf8f6c693f00d57f
david-lorenzo-vargas <dlorenzo.05@gmail.com>
2024-03-06 10:41:36 +0000

    cleaning up

--- Util/formatReferencesPostBody.ts
+++ Util/formatReferencesPostBody.ts
@@ -1,23 +1,28 @@
 import { FormFields } from "../Types/types";
 
-export const formatReferencesPostBody = (obj: FormFields, currentlyWorking: boolean) => {
+export const formatReferencesPostBody = (
+  obj: FormFields,
+  currentlyWorking: boolean
+) => {
   return {
     personal: {
-      "first_name": obj.firstName,
-      "last_name": obj.lastName,
-      "current_address": obj.personalAddress
+      first_name: obj.firstName,
+      last_name: obj.lastName,
+      current_address: obj.personalAddress,
     },
     employer: [
       {
         name: obj.employerName,
-        "start_date": new Date(obj.startDate).getTime(),
-        "end_date": currentlyWorking ? undefined : new Date(obj.endDate).getTime(),
-      }
+        start_date: new Date(obj.startDate).getTime(),
+        end_date: currentlyWorking
+          ? undefined
+          : new Date(obj.endDate).getTime(),
+      },
     ],
     guarantor: {
       name: obj.guarantorName,
       address: obj.guarantorAddress,
-      relation: obj.relationshipToGuarantor
-    }
+      relation: obj.relationshipToGuarantor,
+    },
   };
 };
